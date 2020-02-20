6eb9edaafda658c8113424542ac787e37e6d14c0
Paul Ganssle <paul@ganssle.io>
2020-02-20 14:02:35 -0500

    Reorganize zoneinfo information into class

--- tests/test_zoneinfo.py
+++ tests/test_zoneinfo.py
@@ -10,7 +10,7 @@ from zoneinfo import IANAZone
 
 class IANAZoneTest(unittest.TestCase):
     def _load_local_file(self, key):
-        f = load_zoneinfo_file(key)
+        f = ZoneDumpData.load_zoneinfo_file(key)
         return IANAZone.from_file(f, key=key)
 
     def test_dublin_offsets(self):
@@ -246,70 +246,68 @@ class TZStrTest(unittest.TestCase):
                     self.assertEqual(dt.utcoffset(), expected_tzoffset)
 
 
-###
-#
-# Some example zoneinfo files: These are lzma-compressed and then b85 encoded
-# encoded, to minimize the space taken up in this file
-def load_zoneinfo_file(key):
-    if key not in ZONEINFO_FILES:
-        raise ValueError(f"Zoneinfo file not found: {key}")
+class ZoneDumpData:
+    @classmethod
+    def load_zoneinfo_file(cls, key):
+        if key not in cls.ZONEFILES:
+            raise ValueError(f"Zoneinfo file not found: {key}")
 
-    raw = ZONEINFO_FILES[key]
-    raw = b"".join(map(bytes.strip, raw.split(b"\n")))
-    decoded = base64.b85decode(raw)
-    decompressed = lzma.decompress(decoded)
+        raw = cls.ZONEFILES[key]
+        raw = b"".join(map(bytes.strip, raw.split(b"\n")))
+        decoded = base64.b85decode(raw)
+        decompressed = lzma.decompress(decoded)
 
-    return io.BytesIO(decompressed)
+        return io.BytesIO(decompressed)
 
-
-AMERICA_LOS_ANGELES = b"""
-{Wp48S^xk9=GL@E0stWa8~^|S5YJf5;0qH3OkDsf7KxBg5R*;z{h&-RlhRYu$%jt%!jv+IJxhE=%W1
-?wYb!37Rb?(rgwFIAQI{L#8r*zy!$TMtER_1(vn(Zix^{AVB1(jwr$iL6h0Z!28Gb~UW@0~e512{Z%
-8}Qzdnjl~wJ1{c2>`Z@1A~t&lyL{p{eM{5)QGf7Mo5FW9==mlyXJt2UwpntR7H0eSq!(aYq#aqUz&R
-M*tvuMI)AsM?K3-dV3-TT{t)!Iy#JTo=tXkzAM9~j2YbiOls3(H8Dc>Y|D1aqL51vjLbpYG;GvGTQB
-4bXuJ%mA;(B4eUpu$$@zv2vVcq-Y)VKbzp^teiuzy}R{Luv<C;_cPe*n$Z<jeC9ogWF9=1mvvUYXS>
-DjpuVb`79O+CBmg{Wx!bvx$eu4zRE&PehMb=&G<9$>iZ|bFE)0=4I?KLFGBC0I(0_svgw0%FiMsT%k
-oo*!nEYc6GY@QnU}&4Isg;l=|khi(!VaiSE2=Ny`&&tpi~~;{$u<GHlsr3Ze!iYsU205RFKsLnrXwO
-L?Mq08xffgS{6hE|figx+&N%wbO}re@|}$l;g_6J-Wl%j|qev8A<T?NJ)`;2neGi_DHE4ET*W!c*gg
-PAgU+LE9=bH7;maCUikw^R)UM;TdVvNkQ;FGgN=yQER`SZ1nOgPXr0LCebLety&}kVdmVmB=8eSgtd
-!1%p=a2wooIL!Da}OPXvKBfRo?YxqS>N}%f|7mBhAy;<Er2&_LfND#qXN~Mkgf!@4VFAHr%$c)wrKA
-2cJYWK2>s3YT^sy!$eG~?`9mNJC9@4Bac_p^BZh)Yd_rWW5qh-?tKY(>5VHOL*iT8P@wCavLj^yYbn
-DR+4ukhS+xPrpl)iqB?u)bj9a2aW==g6G3lCJd>(+Blf<d4CF%7utlBUDki}J-!_Dy}5S(MrxSXy~$
-Z+hgH3P^<<w7D72L7I-R%H3(xm&q_DXxkp$owLTS6Wzkhc3nn;laROa3)6hl&gH#)2Lif8fZe$@Cde
-J-Zn&*>r)~^40F4f>cRZ^UF;RibfZ>0m73hRC{$vTfC(STN`g7(B<=Z2556{}0`?p&|Akkst!4Xy4O
-T;A@c$XTUI3FRRjy*KA7uC56FD)z^X{WV*sr(w!c$W357o!&eLO2wTDNOyw@gf(&R<<LF_3URI4=Ei
-`-%dM3T66j#9!aG7&b_@g1-9vo?DzXZ5vGaf~w__p_@_X?OdvQ_r5bvy2hpESTf+{p?jL+!~!{g8-<
--5$@d8EZV&-5@a|;^1gB*R-~{EHFA-td_G2bt;~Y}>t;=-Tu1TV{>%8ZVATC9tjD8|(&`$9YHvZ9bV
-e#>w|8c;Tg|xE&)`*}LwM*E}q}q8^Qja%p`_U)*5DdLI9O@!e=3jFjOCrCq28b_bb;s>%D#iJBCWJi
-{JH!Js;6nfayos$kq^OEX00HO-lokL0!mqm{vBYQl0ssI200dcD
-"""
-
-EUROPE_DUBLIN = b"""
-{Wp48S^xk9=GL@E0stWa8~^|S5YJf5;0>b$_+0=h7KxBg5R*;&J77#T_U2R5sleVWFDmK~Kzj5oh@`
-<njquRZ&tJIS(cXp1>QKHvW^6V{jU-w>qg1tSt0c^vh;?qAqA0%t?;#S~6U8Qiv&f1s9IH#g$m1k1a
-#3+lylw4mwT4QnEUUQdwg+xnEcBlgu31bAVabn41OMZVLGz6NDwG%XuQar!b>GI{qSahE`AG}$kRWb
-uI~JCt;38)Xwbb~Qggs55t+MAHIxgDxzTJ;2xXx99+qCy445kC#v_l8fx|G&jlVvaciR<-wwf22l%4
-(t@S6tnX39#_K(4S0fu$FUs$isu<UOJYm|4)2iaEpsajn@}B#rnY=Cg_TXsm-A)*adXV&$klNTn3n{
-XXlaquu}6m{k%oRmY0Yyhlj*<W{D5m22}OiqnwHT!tnK`wPqx?wiF%v{ipTrOkcJ5P@7OC4(-l`*&S
-B$Wd4Vf8gn?>d<i@%mP*e*ttDj`9M1;9$YV@dhT)DVcwdq(Ly~KDm_&KL?{_mFwwYtJqRZBk)i1FVQ
-y!40w_KyAg?hIA=_{(3#S0eWsF8f%_4Zza$4@$lSmov+Huyn$vP^zJ|8-<C3#q#0kEs9cNg^xUR(m?
-wEWt-DGctAh2nIo~fz%$m$I41=b_WuJ6M9g#A9_Epwqw{d0B|vzmg#_y<=_>9IKzCXB<o`d)**5V6g
-!<<Jw1n5TrN-$)aYz4cLsTmpsUf-6L7ix+kk>78NkARYq@9Dc0TGkhz);NtM_SSzEffNl{2^*CKGdp
-52h!52A)6q9fUSltXF{T*Ehc9Q7u8!W7pE(Fv$D$cKUAt6wY=DA1mGgxC*VXq_If3G#FY6-Voj`fIK
-k`0}Cc72_SD{v>468LV{pyBI33^p0E?}RwDA6Pkq--C~0jF&Z@Pv!dx_1SN_)jwz@P$(oK%P!Tk9?f
-RjK88yxhxlcFtTjjZ$DYssSsa#ufYrR+}}nKS+r384o~!Uw$nwTbF~qgRsgr0N#d@KIinx%<pnyQ!|
->hQB(SJyjJtDtIy(%mDm}ZBGN}dV6K~om|=UVGkbciQ=^$_14|gT21!YQ)@y*Rd0i_lS6gtPBE9+ah
-%WIJPwzUTjIr+J1XckkmA!6WE16%CVAl{Dn&-)=G$Bjh?bh0$Xt1UDcgXJjXzzojuw0>paV~?Sa`VN
-3FysqF<S*L0RYSAY3jt(8wCD04RfyEcP(RNT%x7k(7m-9H3{zuQ`RZy-Rz%*&dldDVFF+TwSAPO1wR
-X^5W5@xJ9{vWw?rc^NH({%Ie<rxKqSVy!Le-_`U&@W_(D+>xTzfKVAu*ucq#+m=|KSSMvp_#@-lwd+
-q*ueFQ^5<D+|jLr?k{O39i8AX2Qb^zi9A<7XD1y!-W2|0Hk8JVkN;gl><|<0R-u4qYMbRqzSn&Q7jS
-uvc%b+EZc%>nI(+&0Tl1Y>a6v4`uNFD-7$QrhHgS7Wnv~rDgfH;rQw3+m`LJxoM4v#gK@?|B{RHJ*V
-xZgk#!p<_&-sjxOda0YaiJ1UnG41VPv(Et%ElzKRMcO$AfgU+Xnwg5p2_+NrnZ1WfEj^fmHd^sx@%J
-WKkh#zaK0ox%rdP)zUmGZZnqmZ_9L=%6R8ibJH0bOT$AGhDo6{fJ?;_U;D|^>5by2ul@i4Zf()InfF
-N}00EQ=q#FPL>RM>svBYQl0ssI200dcD
-"""
-
-ZONEINFO_FILES = {
-    "America/Los_Angeles": AMERICA_LOS_ANGELES,
-    "Europe/Dublin": EUROPE_DUBLIN,
-}
+    ###
+    #
+    # Some example zoneinfo files: These are lzma-compressed and then b85 encoded
+    # encoded, to minimize the space taken up in this file
+    ZONEFILES = {
+        "America/Los_Angeles": b"""
+    {Wp48S^xk9=GL@E0stWa8~^|S5YJf5;0qH3OkDsf7KxBg5R*;z{h&-RlhRYu$%jt%!jv+IJxhE=%
+    W1?wYb!37Rb?(rgwFIAQI{L#8r*zy!$TMtER_1(vn(Zix^{AVB1(jwr$iL6h0Z!28Gb~UW@0~e51
+    2{Z%8}Qzdnjl~wJ1{c2>`Z@1A~t&lyL{p{eM{5)QGf7Mo5FW9==mlyXJt2UwpntR7H0eSq!(aYq#
+    aqUz&RM*tvuMI)AsM?K3-dV3-TT{t)!Iy#JTo=tXkzAM9~j2YbiOls3(H8Dc>Y|D1aqL51vjLbpY
+    G;GvGTQB4bXuJ%mA;(B4eUpu$$@zv2vVcq-Y)VKbzp^teiuzy}R{Luv<C;_cPe*n$Z<jeC9ogWF9
+    =1mvvUYXS>DjpuVb`79O+CBmg{Wx!bvx$eu4zRE&PehMb=&G<9$>iZ|bFE)0=4I?KLFGBC0I(0_s
+    vgw0%FiMsT%koo*!nEYc6GY@QnU}&4Isg;l=|khi(!VaiSE2=Ny`&&tpi~~;{$u<GHlsr3Ze!iYs
+    U205RFKsLnrXwOL?Mq08xffgS{6hE|figx+&N%wbO}re@|}$l;g_6J-Wl%j|qev8A<T?NJ)`;2ne
+    Gi_DHE4ET*W!c*ggPAgU+LE9=bH7;maCUikw^R)UM;TdVvNkQ;FGgN=yQER`SZ1nOgPXr0LCebLe
+    ty&}kVdmVmB=8eSgtd!1%p=a2wooIL!Da}OPXvKBfRo?YxqS>N}%f|7mBhAy;<Er2&_LfND#qXN~
+    Mkgf!@4VFAHr%$c)wrKA2cJYWK2>s3YT^sy!$eG~?`9mNJC9@4Bac_p^BZh)Yd_rWW5qh-?tKY(>
+    5VHOL*iT8P@wCavLj^yYbnDR+4ukhS+xPrpl)iqB?u)bj9a2aW==g6G3lCJd>(+Blf<d4CF%7utl
+    BUDki}J-!_Dy}5S(MrxSXy~$Z+hgH3P^<<w7D72L7I-R%H3(xm&q_DXxkp$owLTS6Wzkhc3nn;la
+    ROa3)6hl&gH#)2Lif8fZe$@CdeJ-Zn&*>r)~^40F4f>cRZ^UF;RibfZ>0m73hRC{$vTfC(STN`g7
+    (B<=Z2556{}0`?p&|Akkst!4Xy4OT;A@c$XTUI3FRRjy*KA7uC56FD)z^X{WV*sr(w!c$W357o!&
+    eLO2wTDNOyw@gf(&R<<LF_3URI4=Ei`-%dM3T66j#9!aG7&b_@g1-9vo?DzXZ5vGaf~w__p_@_X?
+    OdvQ_r5bvy2hpESTf+{p?jL+!~!{g8-<-5$@d8EZV&-5@a|;^1gB*R-~{EHFA-td_G2bt;~Y}>t;
+    =-Tu1TV{>%8ZVATC9tjD8|(&`$9YHvZ9bVe#>w|8c;Tg|xE&)`*}LwM*E}q}q8^Qja%p`_U)*5Dd
+    LI9O@!e=3jFjOCrCq28b_bb;s>%D#iJBCWJi{JH!Js;6nfayos$kq^OEX00HO-
+    lokL0!mqm{vBYQl0ssI200dcD
+    """,
+        "Europe/Dublin": b"""
+    {Wp48S^xk9=GL@E0stWa8~^|S5YJf5;0>b$_+0=h7KxBg5R*;&J77#T_U2R5sleVWFDmK~Kzj5oh
+    @`<njquRZ&tJIS(cXp1>QKHvW^6V{jU-w>qg1tSt0c^vh;?qAqA0%t?;#S~6U8Qiv&f1s9IH#g$m
+    1k1a#3+lylw4mwT4QnEUUQdwg+xnEcBlgu31bAVabn41OMZVLGz6NDwG%XuQar!b>GI{qSahE`AG
+    }$kRWbuI~JCt;38)Xwbb~Qggs55t+MAHIxgDxzTJ;2xXx99+qCy445kC#v_l8fx|G&jlVvaciR<-
+    wwf22l%4(t@S6tnX39#_K(4S0fu$FUs$isu<UOJYm|4)2iaEpsajn@}B#rnY=Cg_TXsm-A)*adXV
+    &$klNTn3n{XXlaquu}6m{k%oRmY0Yyhlj*<W{D5m22}OiqnwHT!tnK`wPqx?wiF%v{ipTrOkcJ5P
+    @7OC4(-l`*&SB$Wd4Vf8gn?>d<i@%mP*e*ttDj`9M1;9$YV@dhT)DVcwdq(Ly~KDm_&KL?{_mFww
+    YtJqRZBk)i1FVQy!40w_KyAg?hIA=_{(3#S0eWsF8f%_4Zza$4@$lSmov+Huyn$vP^zJ|8-<C3#q
+    #0kEs9cNg^xUR(m?wEWt-DGctAh2nIo~fz%$m$I41=b_WuJ6M9g#A9_Epwqw{d0B|vzmg#_y<=_>
+    9IKzCXB<o`d)**5V6g!<<Jw1n5TrN-$)aYz4cLsTmpsUf-6L7ix+kk>78NkARYq@9Dc0TGkhz);N
+    tM_SSzEffNl{2^*CKGdp52h!52A)6q9fUSltXF{T*Ehc9Q7u8!W7pE(Fv$D$cKUAt6wY=DA1mGgx
+    C*VXq_If3G#FY6-Voj`fIKk`0}Cc72_SD{v>468LV{pyBI33^p0E?}RwDA6Pkq--C~0jF&Z@Pv!d
+    x_1SN_)jwz@P$(oK%P!Tk9?fRjK88yxhxlcFtTjjZ$DYssSsa#ufYrR+}}nKS+r384o~!Uw$nwTb
+    F~qgRsgr0N#d@KIinx%<pnyQ!|>hQB(SJyjJtDtIy(%mDm}ZBGN}dV6K~om|=UVGkbciQ=^$_14|
+    gT21!YQ)@y*Rd0i_lS6gtPBE9+ah%WIJPwzUTjIr+J1XckkmA!6WE16%CVAl{Dn&-)=G$Bjh?bh0
+    $Xt1UDcgXJjXzzojuw0>paV~?Sa`VN3FysqF<S*L0RYSAY3jt(8wCD04RfyEcP(RNT%x7k(7m-9H
+    3{zuQ`RZy-Rz%*&dldDVFF+TwSAPO1wRX^5W5@xJ9{vWw?rc^NH({%Ie<rxKqSVy!Le-_`U&@W_(
+    D+>xTzfKVAu*ucq#+m=|KSSMvp_#@-lwd+q*ueFQ^5<D+|jLr?k{O39i8AX2Qb^zi9A<7XD1y!-W
+    2|0Hk8JVkN;gl><|<0R-u4qYMbRqzSn&Q7jSuvc%b+EZc%>nI(+&0Tl1Y>a6v4`uNFD-7$QrhHgS
+    7Wnv~rDgfH;rQw3+m`LJxoM4v#gK@?|B{RHJ*VxZgk#!p<_&-sjxOda0YaiJ1UnG41VPv(Et%Elz
+    KRMcO$AfgU+Xnwg5p2_+NrnZ1WfEj^fmHd^sx@%JWKkh#zaK0ox%rdP)zUmGZZnqmZ_9L=%6R8ib
+    JH0bOT$AGhDo6{fJ?;_U;D|^>5by2ul@i4Zf()InfFN}00EQ=q#FPL>RM>svBYQl0ssI200dcD
+    """,
+    }
