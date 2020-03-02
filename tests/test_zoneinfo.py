45f20c89b0042dfd4425c5e4382a254a975056a8
Paul Ganssle <paul@ganssle.io>
2020-03-02 13:50:30 -0500

    Add tests for repr

--- tests/test_zoneinfo.py
+++ tests/test_zoneinfo.py
@@ -113,6 +113,30 @@ class ZoneInfoTest(TzPathUserMixin, unittest.TestCase):
                 zi_ff = ZoneInfo.from_file(f)
                 self.assertEqual(str(zi_ff), repr(zi_ff))
 
+    def test_repr(self):
+        # The repr is not guaranteed, but I think we can insist that it at
+        # least contain the name of the class.
+        key = next(iter(self.zones()))
+
+        zi = ZoneInfo(key)
+        class_name = "ZoneInfo"
+        with self.subTest(name="from key"):
+            self.assertRegex(repr(zi), class_name)
+
+        file_key = ZONEINFO_DATA.keys[0]
+        file_path = ZONEINFO_DATA.path_from_key(file_key)
+        with open(file_path, "rb") as f:
+            zi_ff = ZoneInfo.from_file(f, key=file_key)
+
+        with self.subTest(name="from file with key"):
+            self.assertRegex(repr(zi_ff), class_name)
+
+        with open(file_path, "rb") as f:
+            zi_ff_nk = ZoneInfo.from_file(f)
+
+        with self.subTest(name="from file without key"):
+            self.assertRegex(repr(zi_ff_nk), class_name)
+
     def test_unambiguous(self):
         test_cases = []
         for key in self.zones():
