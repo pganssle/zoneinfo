a8a54da1b5d2d78d847718da89904423b5236522
Paul Ganssle <paul@ganssle.io>
2020-03-05 17:40:49 -0500

    Add coverage for C extensions
    The hack in the setup.py file is there because `gcov` generates a
    *required* .gcno file at build time at the location of the object files.
    In our case, these object files only ever exist in an ephemeral
    temporary build directory that pip has moved copied all of our files
    into, for build isolation purposes.
    
    This allows you to copy those into some location, specified by the
    GCNO_TARGET_DIR directory, at build time. Maybe in the future we either
    use a different coverage generation tool or figure out a way to relocate
    this .gcno file automatically, without hacking it into setup.py.
    
    Additionally, I should note that the ideal target for this would
    actually be {envtmpdir}, because that's cleared before the environment
    is run each time, but unfortunately it's cleared *after* the install
    command, not before, so anything we copy in there as part of the build
    will get deleted. There is currently no way to insert a command before
    the build occurs to establish our own version of envtmpdir (...with
    blackjack ...and hookers...).

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+
 import setuptools
 from setuptools import Extension
 
@@ -6,3 +8,33 @@ c_extension = Extension(
 )
 
 setuptools.setup(ext_modules=[c_extension])
+
+if "GCNO_TARGET_DIR" in os.environ:
+    import glob
+
+    gcno_files = glob.glob("**/*.gcno", recursive=True)
+
+    if gcno_files:
+        import shutil
+
+        target_dir = os.environ["GCNO_TARGET_DIR"]
+        os.makedirs(target_dir, exist_ok=True)
+        for gcno_file in gcno_files:
+            src = gcno_file
+            src_dir, filename = os.path.split(gcno_file)
+            new_target_dir = target_dir
+
+            # When using gcc-9, the files are created in some flat location
+            # with a naming convention where /path/to/file.gcda would be
+            # represented as ${BASEDIR}/#path#to#file.gcda. In gcc-7, the input
+            # directory is mirrored in the output directory, so the filename
+            # would be ${BASEDIR}/path/to/file.gcda. The gcno files need to
+            # have the same name and relative location as the gcda files,
+            # apparently.
+            if not filename.startswith("#"):
+                rel_src_dir = os.path.relpath(src_dir)
+                new_target_dir = os.path.join(target_dir, rel_src_dir)
+                os.makedirs(new_target_dir, exist_ok=True)
+
+            dst = os.path.join(new_target_dir, filename)
+            shutil.copy(src, dst)
