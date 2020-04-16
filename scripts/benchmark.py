5ae74103b57f9e28cf1a6cdcb39c7a685034fadb
Paul Ganssle <paul@ganssle.io>
2020-04-16 15:46:06 -0400

    Add benchmark for "strong cache" misses
    This assumes that dateutil and both ZoneInfo objects have the same size
    "strong cache" - 8.

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -29,6 +29,8 @@ ZONE_NO_CACHE_CONSTRUCTOR = {
     "dateutil": tz.gettz.nocache,
 }
 
+SOURCES = ["dateutil", "pytz"]
+
 BENCHMARKS = {
     "to_utc": lambda *args, **kwargs: bench_astimezone(
         *args, **kwargs, from_utc=False
@@ -42,6 +44,9 @@ BENCHMARKS = {
     ),
     "no_cache_constructor": lambda *args, **kwargs: bench_constructor(
         *args, **kwargs, cache=False
+    ),
+    "constructor_strong_cache_miss": lambda *args, **kwargs: bench_constructor_strong_cache_miss(
+        *args, **kwargs
     ),
 }
 
@@ -99,6 +104,45 @@ def bench_constructor(source, zone_key, cache=False):
     return func
 
 
+def bench_constructor_strong_cache_miss(source, zone_key):
+    if source == "pytz":
+        raise UnsupportedOperation("pytz does not have a strong and weak cache")
+
+    # Strong cache is 8 keys
+    keys = [
+        "UTC",
+        "America/New_York",
+        "America/Los_Angeles",
+        "America/Chicago",
+        "Asia/Tokyo",
+        "Europe/Lisbon",
+        "Africa/Casablanca",
+        "Australia/Sydney",
+        "America/Denver",
+        "Europe/London",
+    ]
+
+    if zone_key in keys:
+        keys.append("Europe/Moscow")
+    else:
+        keys.append(zone_key)
+
+    constructor = ZONE_DEFAULT_CONSTRUCTOR[source]
+
+    def func(constructor=constructor, keys=keys):
+        for key in keys:
+            constructor(key)
+
+    def setup(_cache=[]):
+        _cache.clear()
+        for key in keys:
+            _cache.append(constructor(key))
+
+    func.setup = setup
+
+    return func
+
+
 @click.command()
 @click.option(
     "-b",
@@ -107,7 +151,7 @@ def bench_constructor(source, zone_key, cache=False):
     multiple=True,
 )
 @click.option(
-    "-c", "--compare", type=click.Choice(["pytz", "dateutil"]), multiple=True
+    "-c", "--compare", type=click.Choice(["all"] + SOURCES), multiple=True
 )
 @click.option("--c_ext/--no_c_ext", default=True)
 @click.option("--py/--no_py", default=True)
@@ -125,8 +169,15 @@ def cli(benchmark, zone, compare, c_ext, py):
     if py:
         sources.append("py_zoneinfo")
 
-    for source in compare:
-        sources.append(source)
+    if len(compare) == 1 and compare[0] == "all":
+        sources.extend(SOURCES)
+    else:
+        for source in compare:
+            if source == "all":
+                raise InvalidInput(
+                    'Cannot specify "all" along with other comparisons.'
+                )
+            sources.append(source)
 
     if not sources:
         raise InvalidInput("Nothing to benchmark specified!")
