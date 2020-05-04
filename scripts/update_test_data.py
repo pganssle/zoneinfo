01092d33770701f2f544bdd88542c8f1871a6d73
Paul Ganssle <paul@ganssle.io>
2020-05-04 11:14:44 -0400

    Improvements to the tzdata update script
    Clears out some dead code and adds metadata to the zoneinfo script.

--- scripts/update_test_data.py
+++ scripts/update_test_data.py
@@ -6,12 +6,20 @@ since we are testing properties of a file that may be subject to change. For
 example, the behavior in the far future of any given zone is likely to change,
 but "does this give the right answer for this file in 2040" is still an
 important property to test.
+
+This must be run from a computer with zoneinfo data installed.
 """
+from __future__ import annotations
+
 import base64
+import functools
 import json
 import lzma
 import pathlib
 import textwrap
+import typing
+
+import zoneinfo
 
 KEYS = [
     "Africa/Abidjan",
@@ -27,69 +35,85 @@ KEYS = [
     "UTC",
 ]
 
-TZ_PATH = pathlib.Path("/usr/share/zoneinfo")
 REPO_ROOT = pathlib.Path(__file__).parent.parent.absolute()
 TEST_DATA_LOC = REPO_ROOT / "tests" / "data"
 
 
-def get_zoneinfo(key):
-    with open(TZ_PATH / key, "rb") as f:
+@functools.lru_cache(maxsize=None)
+def get_zoneinfo_path() -> pathlib.Path:
+    """Get the first zoneinfo directory on TZPATH containing the "UTC" zone."""
+    key = "UTC"
+    for path in map(pathlib.Path, zoneinfo.TZPATH):
+        if (path / key).exists():
+            return path
+    else:
+        raise OSError("Cannot find time zone data.")
+
+
+def get_zoneinfo_metadata() -> typing.Dict[str, str]:
+    path = get_zoneinfo_path()
+
+    tzdata_zi = path / "tzdata.zi"
+    if not tzdata_zi.exists():
+        # tzdata.zi is necessary to get the version information
+        raise OSError("Time zone data does not include tzdata.zi.")
+
+    with open(tzdata_zi, "r") as f:
+        version_line = next(f)
+
+    _, version = version_line.strip().rsplit(" ", 1)
+
+    if (
+        not version[0:4].isdigit()
+        or len(version) < 5
+        or not version[4:].isalpha()
+    ):
+        raise ValueError(
+            "Version string should be YYYYx, "
+            + "where YYYY is the year and x is a letter; "
+            + f"found: {version}"
+        )
+
+    return {"version": version}
+
+
+def get_zoneinfo(key: str) -> bytes:
+    path = get_zoneinfo_path()
+
+    with open(path / key, "rb") as f:
         return f.read()
 
 
-def get_v1_zoneinfo(key):
-    with open(TZ_PATH / key, "rb") as f:
-        return get_v1_zoneinfo_from_file(f)
-
-
-def _get_v1_zoneinfo_from_file(f):
-    """Get a zoneinfo file and ttruncate it to a version 1 file"""
-    if stream.read(4) != b"TZif":
-        raise ValueError("Invalid TZif file: magic not found")
-
-    version = int(stream.read(1))
-
-    if version == 1:
-        f.seek(0)
-        return f.read()
-
-    isutcnt, isstdcnt, leapcnt, timecnt, typecnt, charcnt = struct.unpack(
-        ">6l", stream.read(24)
-    )
-
-    # Any version 2+ file starts with a version 1 file, so we can truncate
-    # after that without issue.
-    file_size = 5 + typecnt * 5 + charcnt * 6 + leapcnt * 8 + isstdcnt + isutcnt
-
-    f.seek(0)
-    out = bytearray(f.read(file_size))
-    out[5] = b"1"
-
-    return bytes(out)
-
-
-def encode_compressed(data):
+def encode_compressed(data: bytes) -> typing.List[str]:
     compressed_zone = lzma.compress(data)
     raw = base64.b85encode(compressed_zone)
 
-    data_str = raw.decode("utf-8")
+    raw_data_str = raw.decode("utf-8")
 
-    data_str = textwrap.wrap(data_str, width=70)
+    data_str = textwrap.wrap(raw_data_str, width=70)
     return data_str
 
 
-def load_compressed_keys():
+def load_compressed_keys() -> typing.Dict[str, typing.List[str]]:
     output = {key: encode_compressed(get_zoneinfo(key)) for key in KEYS}
 
     return output
 
 
-def update_test_data(fname="zoneinfo_data.json"):
+def update_test_data(fname: str = "zoneinfo_data.json") -> None:
     TEST_DATA_LOC.mkdir(exist_ok=True, parents=True)
 
-    json_kwargs = dict(indent=2, sort_keys=True,)
+    # Annotation required: https://github.com/python/mypy/issues/8772
+    json_kwargs: typing.Dict[str, typing.Any] = dict(
+        indent=2, sort_keys=True,
+    )
 
-    output = load_compressed_keys()
+    compressed_keys = load_compressed_keys()
+    metadata = get_zoneinfo_metadata()
+    output = {
+        "metadata": metadata,
+        "data": compressed_keys,
+    }
 
     with open(TEST_DATA_LOC / fname, "w") as f:
         json.dump(output, f, **json_kwargs)
