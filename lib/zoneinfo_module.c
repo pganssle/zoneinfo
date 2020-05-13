3ad8c6230fff36f04bde4c877b3e9291e5a0f94d
Paul Ganssle <paul@ganssle.io>
2020-05-13 13:53:55 -0400

    Add data_tuple type checking

--- lib/zoneinfo_module.c
+++ lib/zoneinfo_module.c
@@ -845,7 +845,14 @@ load_data(PyZoneInfo_ZoneInfo *self, PyObject *file_obj)
     size_t ttinfos_allocated = 0;
 
     data_tuple = PyObject_CallMethod(_common_mod, "load_data", "O", file_obj);
+
     if (data_tuple == NULL) {
+        goto error;
+    }
+
+    if (!PyTuple_CheckExact(data_tuple)) {
+        PyErr_Format(PyExc_TypeError, "Invalid data result type: %r",
+                     data_tuple);
         goto error;
     }
 
