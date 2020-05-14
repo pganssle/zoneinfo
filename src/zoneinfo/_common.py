a21001948edca52bfef2cb23d39edb600e0bf9cf
Paul Ganssle <paul@ganssle.io>
2020-05-14 14:01:42 -0400

    Don't use "raise from" in load_tzdata
    Most of the time the context of the exception is not useful, because
    this only catches exceptions in the fallback data source. It adds a
    bunch of stuff about failing to load the zone from `tzdata` even if
    tzdata is not installed and would be misleading to end users.

--- src/zoneinfo/_common.py
+++ src/zoneinfo/_common.py
@@ -10,7 +10,7 @@ def load_tzdata(key):
 
     try:
         return importlib.resources.open_binary(package_name, resource_name)
-    except (ImportError, FileNotFoundError, UnicodeEncodeError) as e:
+    except (ImportError, FileNotFoundError, UnicodeEncodeError):
         # There are three types of exception that can be raised that all amount
         # to "we cannot find this key":
         #
@@ -21,7 +21,7 @@ def load_tzdata(key):
         #   (e.g. Europe/Krasnoy)
         # UnicodeEncodeError: If package_name or resource_name are not UTF-8,
         #   such as keys containing a surrogate character.
-        raise ZoneInfoNotFoundError(f"No time zone found with key {key}") from e
+        raise ZoneInfoNotFoundError(f"No time zone found with key {key}")
 
 
 def load_data(fobj):
