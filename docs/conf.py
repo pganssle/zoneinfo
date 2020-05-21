08eb2596eb801953d61453f4764e3ab0646fa16b
Paul Ganssle <paul@ganssle.io>
2020-05-21 09:21:31 -0400

    Switch theme to 'nature'

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,26 @@ project = "backports.zoneinfo"
 author = "Paul Ganssle"
 copyright = f"2020, {author}"
 
+# Read the version information from the _version.py file
+def get_version():
+    import ast
+
+    version_line = None
+    with open("../src/backports/zoneinfo/_version.py") as f:
+        for line in f:
+            if line.startswith("__version__ ="):
+                version_line = line
+                break
+
+    if version_line is None:
+        raise ValueError("Version not found!")
+
+    version_str = version_line.split("=", 1)[1].strip()
+
+    return ast.literal_eval(version_str)
+
+
+version = get_version()
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,7 +52,7 @@ exclude_patterns = ["_output", "_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "python_docs_theme"
+html_theme = "nature"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
